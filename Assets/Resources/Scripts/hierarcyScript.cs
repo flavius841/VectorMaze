e719059ba00b49cee26cd9388a293ccf95e6b8c0
flavius841 <flaviusepintilie@gmail.com>
2026-03-20 18:56:52 +0200

    solving the opening maze pt2

--- Assets/Resources/Scripts/hierarcyScript.cs
+++ Assets/Resources/Scripts/hierarcyScript.cs
@@ -3,12 +3,26 @@ using UnityEngine;
 [ExecuteAlways]
 public class hierarcyScript : MonoBehaviour
 {
-    private bool Found = false;
+    private bool Done = false;
+    private int Order;
     void OnValidate()
     {
-        for (int i = 1; i <= transform.childCount; i++)
+        while (!Done)
         {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int.TryParse(transform.GetChild(i).name, out Order);
+                transform.GetChild(i).SetSiblingIndex(Order - 1);
 
+                if (i == transform.childCount - 1)
+                {
+                    Done = true;
+                }
+                else
+                {
+                    Done = false;
+                }
+            }
         }
 
     }
