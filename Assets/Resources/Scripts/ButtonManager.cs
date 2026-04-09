81f36e2e0fb764297e5cb5b6ef0e0182576de7c5
flavius841 <flaviusepintilie@gmail.com>
2026-04-09 13:42:51 +0300

    working on UI

--- Assets/Resources/Scripts/ButtonManager.cs
+++ Assets/Resources/Scripts/ButtonManager.cs
@@ -9,7 +9,8 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] bool StartMovingLefttFor3d;
     [SerializeField] bool StartMovingBackFor2d;
     [SerializeField] bool StartMovingBackFor3d;
-    [SerializeField] bool Goback;
+    [SerializeField] bool Goback2D;
+    [SerializeField] bool Goback3D;
     [SerializeField] float moveSpeed;
     [SerializeField] float MaxXRightFor2D;
     [SerializeField] float MaxXLeftFor3D;
@@ -19,6 +20,8 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] float MaxXRightFor3D;
     [SerializeField] Transform SizePanel2d;
     [SerializeField] Transform SizePanel3d;
+    [SerializeField] GameObject Levels2DPos;
+    [SerializeField] GameObject Levels3DPos;
 
     void Start()
     {
@@ -28,9 +31,14 @@ public class ButtonManager : MonoBehaviour
 
     void Update()
     {
-        Moving(ref StartMovingRightFor2d, true, finalXRightFor2D, Vector3.right, () => SizePanel2d.localPosition.x > MaxXRightFor2D, SizePanel2d);
-        Moving(ref StartMovingLefttFor3d, true, finalXLeftFor3D, Vector3.left, () => SizePanel3d.localPosition.x < MaxXLeftFor3D, SizePanel3d);
-        Moving(ref StartMovingBackFor2d, false, 0f /* doesn't matter */, Vector3.left, () => SizePanel2d.localPosition.x < MaxXLeftFor2D, SizePanel2d);
+        Moving(ref StartMovingRightFor2d, true, finalXRightFor2D, Vector3.right,
+         () => SizePanel2d.localPosition.x > MaxXRightFor2D, SizePanel2d, ref Goback2D);
+        Moving(ref StartMovingLefttFor3d, true, finalXLeftFor3D, Vector3.left,
+         () => SizePanel3d.localPosition.x < MaxXLeftFor3D, SizePanel3d, ref Goback3D);
+        Moving(ref StartMovingBackFor2d, false, 0f /* doesn't matter */, Vector3.left,
+         () => SizePanel2d.localPosition.x < MaxXLeftFor2D, SizePanel2d, ref Goback2D);
+        Moving(ref StartMovingBackFor3d, false, 0f /* doesn't matter */, Vector3.right,
+         () => SizePanel3d.localPosition.x > MaxXRightFor3D, SizePanel3d, ref Goback3D);
     }
 
     public void LoadTutorial()
@@ -46,11 +54,19 @@ public class ButtonManager : MonoBehaviour
     public void AskSizeFor2D()
     {
         StartMovingRightFor2d = true;
+        SizePanel2d.GetComponent<GetPosScript>().enabled = false;
+        SizePanel3d.GetComponent<GetPosScript>().enabled = true;
+        Levels2DPos.GetComponent<GetPosScript>().enabled = false;
+        Levels3DPos.GetComponent<GetPosScript>().enabled = true;
     }
 
     public void AskSizeFor3D()
     {
         StartMovingLefttFor3d = true;
+        SizePanel3d.GetComponent<GetPosScript>().enabled = false;
+        SizePanel2d.GetComponent<GetPosScript>().enabled = true;
+        Levels3DPos.GetComponent<GetPosScript>().enabled = false;
+        Levels2DPos.GetComponent<GetPosScript>().enabled = true;
     }
 
     public void BackFor2d()
@@ -82,7 +98,7 @@ public class ButtonManager : MonoBehaviour
 
     }
 
-    void Moving(ref bool StartMoving, bool Step2nd, float MaxX, float finalX, Vector3 Direction, Func<bool> touchedMax, Transform SizePanel)
+    void Moving(ref bool StartMoving, bool Step2nd, float finalX, Vector3 Direction, Func<bool> touchedMax, Transform SizePanel, ref bool Goback)
     {
         if (StartMoving)
         {
